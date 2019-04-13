f9e536ccb0cf9137d13ff286b13934b1cb363c3d
G <g11v8a@gmail.com>
2019-04-13 16:54:23 +0800

    Encapsulate behavior portals

--- src/components/UserSettingsForm.vue
+++ src/components/UserSettingsForm.vue
@@ -1,30 +1,34 @@
 <template>
-  <form class="card" @submit.prevent="submit">
+  <div class="card">
     <h1 class="text-2xl font-bold mb-6">Your Settings</h1>
     <label class="block mb-6">
-      <span class="form-label mb-2">Email Address</span>
+      <span class="block text-grey-darker font-semibold mb-2">Email Address</span>
       <input v-model="email" class="form-input" placeholder="you@example.com">
     </label>
     <div class="mb-8">
-      <span class="form-label mb-2">Newsletter</span>
+      <span class="block text-grey-darker font-semibold mb-2">Newsletter</span>
       <div class="flex justify-between items-center">
         <p class="mr-4">Send you occasional news and updates.</p>
-        <toggle-input v-model="receiveNewsletter"></toggle-input>
+        <toggle-input v-model="receiveNewsletter" class="flex-no-shrink"/>
       </div>
     </div>
-    <div class="text-right">
+    <div class="flex justify-between">
+      <delete-account-button :account-id="accountId"></delete-account-button>
       <button type="button" class="btn btn-blue" @click="submit">Update Settings</button>
     </div>
-  </form>
+  </div>
 </template>
 
 <script>
 import ToggleInput from './ToggleInput.vue'
+import DeleteAccountButton from './DeleteAccountButton.vue'
 
 export default {
   components: {
     ToggleInput,
+    DeleteAccountButton,
   },
+  props: ['accountId'],
   data() {
     return {
       email: 'jane@example.com',
