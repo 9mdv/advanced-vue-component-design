ce3260e4bd7df38830cffeec2a926be19f423e0e
G <g11v8a@gmail.com>
2019-04-15 20:58:18 +0800

    Compound Components and ProvideInject

--- src/App.vue
+++ src/App.vue
@@ -137,9 +137,37 @@
       <profile-card></profile-card>
     </div>
 
-    <div style="width: 300px" class="border-l border-r border-dashed border-grey-dark py-4 mx-auto mt-8">
+    <div
+      style="width: 300px"
+      class="border-l border-r border-dashed border-grey-dark py-4 mx-auto mt-8"
+    >
       <profile-card></profile-card>
     </div>
+
+    <!-- Accordion -->
+    <div class="max-w-sm mx-auto mt-8">
+      <accordion-list>
+        <accordion-item :item-id="1">
+          <template slot="header">Item A</template>
+          <template
+            slot="content"
+          >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec finibus auctor massa, vitae posuere sem tincidunt eget. Duis non vestibulum diam. Pellentesque eleifend interdum mollis. Donec porttitor dui enim. Maecenas a semper sem. Nunc molestie turpis vitae sem auctor, non venenatis velit ultricies.</template>
+        </accordion-item>
+        <accordion-item :item-id="2">
+          <template slot="header">Item B</template>
+          <template
+            slot="content"
+          >Vestibulum nec urna non nisi viverra vehicula ut sed metus. Cras scelerisque condimentum nunc ut mattis. Curabitur euismod sagittis efficitur. Cras vel orci consectetur, ullamcorper magna vitae, sodales mauris. Aliquam tempus volutpat mi. Nunc vitae molestie ante. In sed varius tortor.</template>
+        </accordion-item>
+        <accordion-item :item-id="3">
+          <template slot="header">Item C</template>
+          <template
+            slot="content"
+          >Sed porttitor mattis egestas. Duis nec rhoncus ligula. Etiam pellentesque mattis mauris. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Maecenas vel dapibus nulla. Phasellus lobortis nibh et faucibus tincidunt. Phasellus non iaculis turpis, vitae gravida lacus.</template>
+        </accordion-item>
+      </accordion-list>
+    </div>
+    <!-- End accordion -->
   </div>
 </template>
 
@@ -158,6 +186,8 @@ import StackedTagInput from './components/StackedTagInput.vue'
 import InlineTagInput from './components/InlineTagInput.vue'
 import WithDimensions from './components/WithDimensions.vue'
 import ProfileCard from './components/ProfileCard.vue'
+import AccordionList from './components/AccordionList.vue'
+import AccordionItem from './components/AccordionItem.vue'
 
 export default {
   data() {
@@ -183,6 +213,8 @@ export default {
     InlineTagInput,
     WithDimensions,
     ProfileCard,
+    AccordionList,
+    AccordionItem,
   },
 }
 </script>
