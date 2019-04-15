b351dcd42fb1371ca9cd25cd605d930656750dbd
G <g11v8a@gmail.com>
2019-04-16 07:28:30 +0800

    Building a Search Select Click Outside Component

--- src/components/SearchSelect.vue
+++ src/components/SearchSelect.vue
@@ -1,39 +1,46 @@
 <template>
-  <div class="search-select" :class="{ 'is-active': isOpen }">
-    <button ref="button" @click="open" type="button" class="search-select-input">
-      <span v-if="value !== null">{{ value }}</span>
-      <span v-else class="search-select-placeholder">Select a band...</span>
-    </button>
-    <div v-show="isOpen" class="search-select-dropdown">
-      <input
-        class="search-select-search"
-        v-model="search"
-        ref="search"
-        @keydown.esc="close"
-        @keydown.up="highlightPrev"
-        @keydown.down="highlightNext"
-        @keydown.enter.prevent="selectHighlighted"
-        @keydown.tab.prevent
-      >
-      <ul ref="options" v-show="filteredOptions.length > 0" class="search-select-options">
-        <li
-          class="search-select-option"
-          v-for="(option, i) in filteredOptions"
-          :key="option"
-          @click="select(option)"
-          :class="{ 'is-active': i === highlightedIndex}"
-        >{{ option }}</li>
-      </ul>
-      <div
-        v-show="filteredOptions.length === 0"
-        class="search-select-empty"
-      >No results found for "{{ search }}"</div>
+  <on-click-outside :do="close">
+    <div class="search-select" :class="{ 'is-active': isOpen }">
+      <button ref="button" @click="open" type="button" class="search-select-input">
+        <span v-if="value !== null">{{ value }}</span>
+        <span v-else class="search-select-placeholder">Select a band...</span>
+      </button>
+      <div v-show="isOpen" class="search-select-dropdown">
+        <input
+          class="search-select-search"
+          v-model="search"
+          ref="search"
+          @keydown.esc="close"
+          @keydown.up="highlightPrev"
+          @keydown.down="highlightNext"
+          @keydown.enter.prevent="selectHighlighted"
+          @keydown.tab.prevent
+        >
+        <ul ref="options" v-show="filteredOptions.length > 0" class="search-select-options">
+          <li
+            class="search-select-option"
+            v-for="(option, i) in filteredOptions"
+            :key="option"
+            @click="select(option)"
+            :class="{ 'is-active': i === highlightedIndex}"
+          >{{ option }}</li>
+        </ul>
+        <div
+          v-show="filteredOptions.length === 0"
+          class="search-select-empty"
+        >No results found for "{{ search }}"</div>
+      </div>
     </div>
-  </div>
+  </on-click-outside>
 </template>
 
 <script>
+import OnClickOutside from './OnClickOutside.vue'
+
 export default {
+  components: {
+    OnClickOutside,
+  },
   props: ['value', 'options', 'filterFunction'],
   data() {
     return {
@@ -59,6 +66,9 @@ export default {
       })
     },
     close() {
+      if (!this.isOpen) {
+        return
+      }
       this.isOpen = false
       this.$refs.button.focus()
     },
